373c50a1eaddf6008ace4f8c2c70ea997e58f3bd
Niels Balemans <balemans.niels@gmail.com>
2016-10-20 10:00:45 +0200

    Working Knoppen

--- DigitaleKlok/isim/Knoppen_tb_isim_beh.exe.sim/work/Knoppen_tb_isim_beh.exe_main.c
+++ DigitaleKlok/isim/Knoppen_tb_isim_beh.exe.sim/work/Knoppen_tb_isim_beh.exe_main.c
@@ -26,6 +26,7 @@ int main(int argc, char **argv)
     xsi_register_min_prec_unit(-12);
     ieee_p_2592010699_init();
     work_a_1473390551_3212880686_init();
+    work_a_2568520686_3212880686_init();
     work_a_0766807436_3212880686_init();
     work_a_1092745091_2372691052_init();
 
