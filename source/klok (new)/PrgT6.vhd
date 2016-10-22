8d55d62633634c521f89056d57654f155d6f8572
Dieter97 <dieter.balemans@hotmail.com>
2016-10-22 15:12:27 +0200

    Chagnes To PrgT6

--- source/klok (new)/PrgT6.vhd
+++ source/klok (new)/PrgT6.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
 -- Company: 
--- Engineer: 
+-- Engineer: Dieter - Niels Balemans
 -- 
 -- Create Date:    20:21:53 10/20/2016 
 -- Design Name: 
@@ -20,21 +20,13 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity PrgT6 is
 	Generic(min1,min2,min3,min4,min5,min6 : std_logic_vector(3 downto 0) := x"0";
 			  max1,max2,max3,max4,max5,max6 : std_logic_vector(3 downto 0) := x"9");
     Port ( sysclk : in  STD_LOGIC;
            cnten : in  STD_LOGIC;
            reset : in  STD_LOGIC;
+			  updwn : in STD_LOGIC;
            count : out  STD_LOGIC_VECTOR (23 downto 0);
            tc : out  STD_LOGIC);
 end PrgT6;
@@ -62,6 +54,7 @@ begin
 	Port map(sysclk => sysclk,
 				cnten => cnten,
 				reset => reset,
+				updwn => updwn,
 				count => count(3 downto 0),
 				tc => tcT1CntenT2);
 				
@@ -71,6 +64,7 @@ begin
 	Port map(sysclk => sysclk,
 				cnten => tcT1CntenT2,
 				reset => reset,
+				updwn => updwn,
 				count => count(7 downto 4),
 				tc => tcT2CntenT3);
 				
@@ -80,6 +74,7 @@ begin
 	Port map(sysclk => sysclk,
 				cnten => tcT2CntenT3,
 				reset => reset,
+				updwn => updwn,
 				count => count(11 downto 8),
 				tc => tcT3CntenT4);
 	
@@ -89,6 +84,7 @@ begin
 	Port map(sysclk => sysclk,
 				cnten => tcT3CntenT4,
 				reset => reset,
+				updwn => updwn,
 				count => count(15 downto 12),
 				tc => tcT4CntenT5);
 				
@@ -98,6 +94,7 @@ begin
 	Port map(sysclk => sysclk,
 				cnten => tcT4CntenT5,
 				reset => reset,
+				updwn => updwn,
 				count => count(19 downto 16),
 				tc => tcT5CntenT6);
 				
@@ -107,6 +104,7 @@ begin
 	Port map(sysclk => sysclk,
 				cnten => tcT5CntenT6,
 				reset => reset,
+				updwn => updwn,
 				count => count(23 downto 20),
 				tc => tc);	
 
