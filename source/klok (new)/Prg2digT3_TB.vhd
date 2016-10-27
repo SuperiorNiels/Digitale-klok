a7e948b7564b20f315c60660713965488487d561
Dieter97 <dieter.balemans@hotmail.com>
2016-10-27 09:09:06 +0200

    Added Prg2digT3
    Dit is de definitieve teller. DEZE GEBRUIKEN!!!!!
    NIET T6!!!!!

---
+++ source/klok (new)/Prg2digT3_TB.vhd
@@ -0,0 +1,105 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: Dieter Balemans
+--
+-- Create Date:   09:00:37 10/27/2016
+-- Design Name:   
+-- Module Name:   C:/Users/Dieter/OneDrive/Documenten/Electronica/klok/klok/Prg2digT3_TB.vhd
+-- Project Name:  klok
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: Prg2digT3
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY Prg2digT3_TB IS
+END Prg2digT3_TB;
+ 
+ARCHITECTURE behavior OF Prg2digT3_TB IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Prg2digT3
+    PORT(
+         sysclk : IN  std_logic;
+         cnten : IN  std_logic;
+         reset : IN  std_logic;
+         updwn : IN  std_logic;
+         count : OUT  std_logic_vector(23 downto 0);
+         tc : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal sysclk : std_logic := '0';
+   signal cnten : std_logic := '0';
+   signal reset : std_logic := '1';
+   signal updwn : std_logic := '0';
+
+ 	--Outputs
+   signal count : std_logic_vector(23 downto 0);
+   signal tc : std_logic;
+
+   -- Clock period definitions
+   constant sysclk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Prg2digT3 PORT MAP (
+          sysclk => sysclk,
+          cnten => cnten,
+          reset => reset,
+          updwn => updwn,
+          count => count,
+          tc => tc
+        );
+
+   -- Clock process definitions
+   sysclk_process :process
+   begin
+		sysclk <= '0';
+		wait for sysclk_period/2;
+		sysclk <= '1';
+		wait for sysclk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+		reset <= '0'; cnten <= '1'; updwn <= '1';
+		wait for 1000 ns;
+		reset <= '0'; cnten <= '1'; updwn <= '0';
+		wait for 1000 ns;
+		reset <= '1'; cnten <= '1'; updwn <= '1';
+		wait for 1000 ns;
+		reset <= '0'; cnten <= '1'; updwn <= '1';
+		wait for 1000 ns;
+      wait;
+   end process;
+
+END;
---
+++ source/klok (new)/Prg2digT6.vhd
@@ -0,0 +1,89 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: Dieter Balemans
+-- 
+-- Create Date:    08:42:22 10/27/2016 
+-- Design Name: 
+-- Module Name:    Prg2digT3 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Prg2digT3 is
+	Generic(	min1,min2,min3 : std_logic_vector(7 downto 0) := x"00";
+				max1,max2,max3 : std_logic_vector(7 downto 0) := x"23");
+    Port ( sysclk : in  STD_LOGIC;
+           cnten : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           updwn : in  STD_LOGIC;
+           count : out  STD_LOGIC_VECTOR (23 downto 0);
+           tc : out  STD_LOGIC);
+end Prg2digT3;
+
+architecture Behavioral of Prg2digT3 is
+	Component PrgTeller2dig
+		Generic (bcd_min		:	std_logic_vector(7 downto 0):= x"00";
+					bcd_max		:	std_logic_vector(7 downto 0):= x"99");
+		port (cnten			:	in  std_logic;
+				updwn			:	in  std_logic := '1';
+				reset 		: 	in  std_logic;
+				sysclk 		: 	in  std_logic;
+				bcd_cnt		:	out std_logic_vector(7 downto 0);
+				tc 			:	out std_logic);
+		end component;
+				
+	--interne signalen
+	signal tcT1CntenT2 : std_logic := '0';
+	signal tcT2CntenT3 : std_logic := '0';
+begin
+	T1 : PrgTeller2dig
+		Generic map(bcd_min => min1,
+						bcd_max => max1)
+		Port map(sysclk => sysclk,
+					cnten => cnten,
+					updwn => updwn,
+					reset => reset,
+					bcd_cnt => count(7 downto 0),
+					tc => tcT1CntenT2);
+
+	T2 : PrgTeller2dig
+		Generic map(bcd_min => min2,
+						bcd_max => max2)
+		Port map(sysclk => sysclk,
+					cnten => tcT1CntenT2,
+					updwn => updwn,
+					reset => reset,
+					bcd_cnt => count(15 downto 8),
+					tc => tcT2CntenT3);
+					
+	T3 : PrgTeller2dig
+		Generic map(bcd_min => min3,
+						bcd_max => max3)
+		Port map(sysclk => sysclk,
+					cnten => tcT2CntenT3,
+					updwn => updwn,
+					reset => reset,
+					bcd_cnt => count(23 downto 16),
+					tc => tc);
+end Behavioral;
+
