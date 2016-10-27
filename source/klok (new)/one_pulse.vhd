8466855e88a23c0a3446b8b4cf2f012718d04aa0
Niels Balemans <balemans.niels@gmail.com>
2016-10-27 08:57:58 +0200

    Made one_pulse easy

--- source/klok (new)/one_pulse.vhd
+++ source/klok (new)/one_pulse.vhd
@@ -37,30 +37,16 @@ end one_pulse;
 
 
 ARCHITECTURE behavioral of one_pulse is
-	type state is (wait1,outp1,wait0);								-- wait1 : wacht op 1 op inp, wait0 : wacht op 0 op inp, outp1 : outp staat op 1
-	signal present_state, next_state : state;
+signal delay1, delay2, delay3: STD_LOGIC;
 begin
-	STATE_REG: process (sysclk)
-	begin
-		if rising_edge(sysclk) then present_state <= next_state; end if;
+	process(sysclk)
+		begin
+		if rising_edge(sysclk) then
+			delay1 <= inp;
+			delay2 <= delay1;
+			delay3 <= delay2;
+		end if;
 	end process;
-	NXT_STATE: process (present_state, inp)		
-	begin
-		case present_state is
-			when wait1 =>	if inp = '1' then 	next_state <= outp1;	else next_state <= wait1;	end if;
-			when outp1 =>	next_state <= wait0;
-			when wait0 =>	if inp = '0' then 	next_state <= wait1;	else next_state <= wait0;	end if;		
-		end case;
-	end process;
-	OUTPUTS : process (sysclk,present_state) 		
-	begin	
-		if rising_edge(sysclk) then 								
-			case present_state is
-				when wait1 	=> outp <= '0';
-				when outp1  =>	outp <= '1';
-				when wait0  =>	outp <= '0';
-			end case;
-		end if;	
-	end process;	
+	outp <= delay1 and delay2 and not delay3;
 end Behavioral;	
 
